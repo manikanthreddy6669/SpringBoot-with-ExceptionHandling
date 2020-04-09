54ef6fb9540ad58f52661b8e35a4afd2e8539476
manikanthreddy6669 <manikanthreddy669@gmail.com>
2020-04-09 15:54:46 +0530

    SpringBootProject with Exceptions

---
+++ ECrud-Exceptions/src/main/java/com/example/globalexception/GlobalException.java
@@ -0,0 +1,17 @@
+package com.example.globalexception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import com.example.exceptions.IdNotFoundException;
+
+@ControllerAdvice
+public class GlobalException {
+
+	@ExceptionHandler(IdNotFoundException.class) 
+	public ResponseEntity<String> employeeNotFound(IdNotFoundException e){ 
+		return new ResponseEntity<String>(e.getMessage(),new HttpHeaders(),HttpStatus.NOT_FOUND); 
+	}
+}
